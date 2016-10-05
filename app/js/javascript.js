4229f173621404d72ce797f53081f54c12455fbb
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-05 15:40:00 -0700

    setup basic ajax

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,19 +1,31 @@
 $( document ).ready(function(){
-  const w = 1300;
+  const w = 600;
   const h = 600;
   const margin = {
-    top: 50,
+    top: 90,
     bottom: 90,
-    left: 100,
-    right: 30
+    left: 90,
+    right: 90
   }
 
   function title(){
   }
-  function render(base, rawData){
-    }
+  function render(data){
+    const width = w - (margin.left + margin.right);
+    const height = h - (margin.up + margin.down);
+
+    const svg = d3.select("#canvas")
+                  .append("svg")
+                  .attr("id","chart")
+                  .attr("width", w)
+                  .attr("height", h)
+
+    const chart = svg.append("g")
+                    .classed("display", true)
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    console.log(data)
   }
-  const url = '';
+  const url = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json';
   $.ajax({
     type: "GET",
     dataType: "json",
@@ -23,7 +35,7 @@ $( document ).ready(function(){
     complete: () =>{
     },
     success: data =>{
-      console.log(data)
+      render(data)
     },
     fail: () =>{
       console.log('failure!')
